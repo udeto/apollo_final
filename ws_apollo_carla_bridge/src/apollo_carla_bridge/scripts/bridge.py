267c353bcb62bddee1037053a4f8b79cbaee3261
Lukas <thede@fzi.de>
2019-08-10 02:00:12 +0200

    Fixed camera short and long, image to apollo

--- ws_apollo_carla_bridge/src/apollo_carla_bridge/scripts/bridge.py
+++ ws_apollo_carla_bridge/src/apollo_carla_bridge/scripts/bridge.py
@@ -15,6 +15,7 @@ CARLA_CAMERA_SHORT_TOPIC = '/camera_short/image_raw'
 APOLLO_LIDAR_TOPIC = '/apollo/sensor/velodyne64/compensator/PointCloud2'
 APOLLO_CAMERA_LONG_TOPIC = '/apollo/sensor/camera/traffic/image_long'
 APOLLO_CAMERA_SHORT_TOPIC = '/apollo/sensor/camera/obstacle/front_6mm'
+APOLLO_CAMERA_SHORT_TOPIC2 = '/apollo/sensor/camera/traffic/image_short'
 
 def forward_callback(data, pub):
     pub.publish(data)
@@ -27,10 +28,13 @@ def main():
     pub_lidar = rospy.Publisher(APOLLO_LIDAR_TOPIC, PointCloud2, queue_size=10)
     rospy.Subscriber(CARLA_LIDAR_TOPIC, PointCloud2, forward_callback, pub_lidar, queue_size=10)
 
-    # pub_cam_long = rospy.Publisher(APOLLO_CAMERA_LONG_TOPIC, Image, queue_size=10)
-    # rospy.Subscriber(CARLA_CAMERA_LONG_TOPIC, Image, forward_callback, pub_cam_long, queue_size=10)
+    pub_cam_long = rospy.Publisher(APOLLO_CAMERA_LONG_TOPIC, Image, queue_size=10)
+    rospy.Subscriber(CARLA_CAMERA_LONG_TOPIC, Image, forward_callback, pub_cam_long, queue_size=10)
 
     pub_cam_short = rospy.Publisher(APOLLO_CAMERA_SHORT_TOPIC, Image, queue_size=10)
+    rospy.Subscriber(CARLA_CAMERA_SHORT_TOPIC, Image, forward_callback, pub_cam_short, queue_size=10)
+
+    pub_cam_short = rospy.Publisher(APOLLO_CAMERA_SHORT_TOPIC2, Image, queue_size=10)
     rospy.Subscriber(CARLA_CAMERA_SHORT_TOPIC, Image, forward_callback, pub_cam_short, queue_size=10)
 
     control_transform.setup()
